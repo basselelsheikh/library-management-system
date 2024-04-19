91ba711757c3f2a09fade360cd814de73871c518
Bassel El-Sheikh <bassel.elsheikh@gmail.com>
2024-04-19 12:40:00 +0300

    fixed Jpa error in Patron model

--- src/main/java/com/example/library/model/Patron.java
+++ src/main/java/com/example/library/model/Patron.java
@@ -1,9 +1,11 @@
 package com.example.library.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Patron {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
